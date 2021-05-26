537d4a10c3927722779cebf9dfa02e3dec5eaa12
David <david.pok@gmail.com>
2021-05-26 16:14:05 -0700

    sets up client side builds

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
 const server = express();
 
 server.use(express.json());
+
+//this will use path to load our static assets
+//at the destination
+server.use(express.static(path.join(__dirname, "client/build")));
 
 if (process.env.NODE_ENV === "development") {
   //on heroku machine an env variable is called
@@ -14,8 +19,15 @@ if (process.env.NODE_ENV === "development") {
   server.use(cors());
 }
 
-server.use("*", (req, res) => {
-  res.send(`<h1>App is running!</h1>`);
+server.get("/api/hello", (req, res) => {
+  res.json({ message: "hello" });
+});
+
+//make sure to run npm run build in client directory first
+//this catch all will just send back index.html
+//if an endpoint that doesn't exist is hit
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 4000;
